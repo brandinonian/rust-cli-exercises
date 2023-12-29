e6591ff87946404b4ccc3e99751df45385b8ba8b
Brandon Phillips <brandinonian@gmail.com>
2023-12-29 10:57:34 -0700

    working on 02

--- 02_bin_files/src/main.rs
+++ 02_bin_files/src/main.rs
@@ -1,3 +1,29 @@
+mod fn1 {
+    pub fn fn1(number: i32) -> i32 {
+        return number;
+    }
+}
+
+
+mod fn2 {
+    pub fn fn2(number: i32) -> i32 {
+        return number + 1;
+    }
+}
+
+
+mod fn3 {
+    pub fn fn3(number: i32) -> i32 {
+        return number + 2;
+    }
+}
+
+
 fn main() {
-    todo!("replace this line with your code!")
+    
+    let number = 4;
+
+    let sum = fn1::fn1(number) + fn2::fn2(number) + fn3::fn3(number);
+
+    println!("{}", sum);
 }
