af7f3813921f02154e2d73d1637f9af77286b06a
Brandon Phillips <brandinonian@gmail.com>
2023-12-30 09:04:25 -0700

    work on 03

--- 03_arg_parse/src/main.rs
+++ 03_arg_parse/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
 fn main() {
-    todo!("replace this line with your code!")
+    // get arguments
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 3 {
+        panic!("Too many arguments passed.");
+    } else if args.len() > 2 {
+        let arg1 = args[1].clone();
+        let arg2 = args[2].clone();
+
+        if arg1 == "--flag" || arg1 == "-f" {
+            println!("{}", arg2);
+        } else {
+            println!("{}", arg1);
+            println!("{}", arg2);
+        }
+    } else {
+        let arg1 = args[1].clone();
+        println!("{}", arg1);
+    }
 }
